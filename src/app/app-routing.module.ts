05c59e20cab0b1695eb06341bbd504d8f0cf2a7f
Kotha Nikhil <kothanikhil@gmail.com>
2024-12-22 12:53:27 +0530

    Cleaned up dashboard to start from beginning.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' }, // Redirect to dashboard as default
-  { path: '**', redirectTo: '/dashboard' }, // Redirect any unknown path to dashboard
 ];
 
 @NgModule({
